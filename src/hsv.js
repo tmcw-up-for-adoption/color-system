174f6feb36b09556f1f298376164c25c5234ede5
Tom MacWright <tom@macwright.org>
2013-09-10 11:23:55 -0400

    invert hsv

--- src/hsv.js
+++ src/hsv.js
@@ -5,3 +5,22 @@ var hue = require('./hue'),
 module.exports = function hsl(rgb) {
     return [hue(rgb), saturation(rgb), value(rgb)];
 };
+
+module.exports.invert = function rgb(hsv) {
+    if (!hsv[1]) return [hsv[2] * 255, hsv[2] * 255, hsv[2] * 255];
+    var h = hsv[0], s = hsv[1], v = hsv[2] * 255;
+    h /= 60;
+    var i = Math.floor(h),
+        f = h - i,
+        p = v * (1 - s),
+        q = v * (1 - s * f),
+        t = v * (1 - s * (1 - f));
+    switch (i) {
+        case 0: return [v, t, p];
+        case 1: return [q, v, p];
+        case 2: return [p, v, t];
+        case 3: return [p, q, v];
+        case 4: return [t, p, v];
+        case 5: return [v, p, q];
+    }
+};
--- test/hsv.js
+++ test/hsv.js
@@ -7,4 +7,8 @@ describe('hsv', function() {
         expect(systems.hsv([0, 255, 0])).to.eql([120, 1, 1]);
         expect(systems.hsv([0, 0, 255])).to.eql([240, 1, 1]);
     });
+    it('invert', function() {
+        expect(systems.hsv.invert([0, 1, 1])).to.eql([255, 0, 0]);
+        expect(systems.hsv.invert([120, 1, 1])).to.eql([0, 255, 0]);
+    });
 });
