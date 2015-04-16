78297030eefdf47105fcea6787673bf50445a47f
Jacob Parker <jacobparker1992@gmail.com>
2015-04-16 19:34:25 +0100

    CYMK fixes and tests, make expept-approx use 2sf

--- test/cmyk.js
+++ test/cmyk.js
@@ -1,10 +1,37 @@
 var systems = require('../'),
-    expect = require('expect.js');
+    expect = require('./expect-aprx');
 
 describe('cmyk', function() {
     it('forward', function() {
-        expect(systems.cmyk([0, 0, 0])).to.eql([0, 0, 0, 1]);
-        expect(systems.cmyk([255, 0, 0])).to.eql([0, 1, 1, 0]);
-        expect(systems.cmyk([0, 255, 0])).to.eql([1, 0, 1, 0]);
+        expect(systems.cmyk([0, 0, 0])).to.aprx([0, 0, 0, 1]);
+        expect(systems.cmyk([128, 128, 128])).to.aprx([0, 0, 0, 0.5]);
+        expect(systems.cmyk([255, 255, 255])).to.aprx([0, 0, 0, 0]);
+
+        expect(systems.cmyk([128, 0, 0])).to.aprx([0, 1, 1, 0.5]);
+        expect(systems.cmyk([128, 128, 0])).to.aprx([0, 0, 1, 0.5]);
+        expect(systems.cmyk([0, 128, 0])).to.aprx([1, 0, 1, 0.5]);
+        expect(systems.cmyk([0, 128, 128])).to.aprx([1, 0, 0, 0.5]);
+        expect(systems.cmyk([0, 0, 128])).to.aprx([1, 1, 0, 0.5]);
+        expect(systems.cmyk([128, 0, 128])).to.aprx([0, 1, 0, 0.5]);
+
+        expect(systems.cmyk([255, 0, 0])).to.aprx([0, 1, 1, 0]);
+        expect(systems.cmyk([0, 255, 0])).to.aprx([1, 0, 1, 0]);
+        expect(systems.cmyk([0, 0, 255])).to.aprx([1, 1, 0, 0]);
+    });
+    it('backward', function() {
+        expect(systems.cmyk.invert([0, 0, 0, 1])).to.aprx([0, 0, 0]);
+        expect(systems.cmyk.invert([0, 0, 0, 0.5])).to.aprx([128, 128, 128]);
+        expect(systems.cmyk.invert([0, 0, 0, 0])).to.aprx([255, 255, 255]);
+
+        expect(systems.cmyk.invert([0, 1, 1, 0.5])).to.aprx([128, 0, 0]);
+        expect(systems.cmyk.invert([0, 0, 1, 0.5])).to.aprx([128, 128, 0]);
+        expect(systems.cmyk.invert([1, 0, 1, 0.5])).to.aprx([0, 128, 0]);
+        expect(systems.cmyk.invert([1, 0, 0, 0.5])).to.aprx([0, 128, 128]);
+        expect(systems.cmyk.invert([1, 1, 0, 0.5])).to.aprx([0, 0, 128]);
+        expect(systems.cmyk.invert([0, 1, 0, 0.5])).to.aprx([128, 0, 128]);
+
+        expect(systems.cmyk.invert([0, 1, 1, 0])).to.aprx([255, 0, 0]);
+        expect(systems.cmyk.invert([1, 0, 1, 0])).to.aprx([0, 255, 0]);
+        expect(systems.cmyk.invert([1, 1, 0, 0])).to.aprx([0, 0, 255]);
     });
 });
--- test/expect-aprx.js
+++ test/expect-aprx.js
@@ -4,13 +4,13 @@ expect.Assertion.prototype.aprx = function aprx(color) {
     var success;
 
     if (Array.isArray(this.obj)) {
-        success = this.obj.every(function(value, index) {
-            var factor = (value < 1) ? 255 : 1;
+        success = (this.obj.length === color.length) && this.obj.every(function(value, index) {
+            var factor = (value < 1) ? 100 : 1;
 
             return Math.round(value * factor) === Math.round(color[index] * factor);
         });
     } else {
-        var factor = (this.obj < 1) ? 255 : 1;
+        var factor = (this.obj < 1) ? 100 : 1;
 
         success = Math.round(this.obj * factor) === Math.round(color * factor);
     }
